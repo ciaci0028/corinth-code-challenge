422573767b76239d5f920b64ff04053c6879ed1f
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-02-16 16:05:19 -0600

    added saga for making request

---
+++ src/redux/sagas/root.saga.js
@@ -0,0 +1,32 @@
+import { all } from 'redux-saga/effects';
+import { axios } from 'axios';
+import { put, takeEvery } from 'redux-saga/effects';
+
+// Function of listeners to activate the requests
+function* sagaListeners() {
+    yield takeEvery('FETCH_SEARCHED_CHARACTER', fetchCharacter);
+}
+// Saga for retrieving character from SWAPI
+function* fetchCharacter(action) {
+    try {
+        const response = yield axios.get(`/search/`);
+        yield put({
+            type: 'SET_CHARACTER',
+            payload: response.data
+        })
+    }
+    catch (error) {
+        console.log('failed to fetch character', error);
+    }
+};
+
+
+
+
+export default function* rootSaga() {
+    yield all([
+        fetchCharacter
+    ]);
+};
+
+export default rootSaga;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
-import rootReducer from './reducers/store.reducer'; // imports ./redux/reducers/index.js
+import rootReducer from './reducers/store.reducer';
+import rootSaga from './sagas/root.saga';
+
+// Saga Middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// this line creates an array of all of redux middleware to use
+// I don't want a whole ton of console logs in the production code
+const middlewareList = process.env.NODE_ENV === 'development' ?
+    [sagaMiddleware, logger] :
+    [sagaMiddleware];
 
 // Creating a store. In theory, if I had more reducers I'd be able
 // to organize them into different parts
@@ -11,5 +22,8 @@ const store = createStore(
     applyMiddleware(...middlewareList),
 );
 
+// tells the saga middleware to use the rootSaga
+// rootSaga contains all of our other sagas
+sagaMiddleware.run(rootSaga);
 
 export default store;
